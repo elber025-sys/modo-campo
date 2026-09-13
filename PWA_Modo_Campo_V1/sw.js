65dc2622a0773fe53345987d6aa9838b463d0faa
elber025-sys <elber025@gmail.com>
2026-09-12 23:33:59 -0300

    Add files via upload

--- PWA_Modo_Campo_V1/sw.js
+++ PWA_Modo_Campo_V1/sw.js
@@ -1,4 +1,4 @@
-const SHELL='modo-campo-v8-2';
+const SHELL='modo-campo-v8-3';
 const TILE='campo-tiles-v2';
 const SAT='campo-satelite-eox-v8';
 const ASSETS=['./','./index.html','./manifest.webmanifest','https://unpkg.com/leaflet@1.9.4/dist/leaflet.css','https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'];
